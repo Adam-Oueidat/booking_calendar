922ae8c7fdf00865a9399ed7447a86e42d6d3ea5
Adam Oueidat <ooueidat@gmail.com>
2024-06-18 23:46:14 +0200

    TODO list for my fun idea

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -10,6 +10,13 @@ export default function CalendarPage() {
             <h1 className="text-4xl font-semibold text-center">
               Tåg biljetter
             </h1>
+            {/* TODO
+            - Add input fields for departure and arrival stations
+            - Use the fields to create a link to SJ website
+            - Perhaps scrape that page to show different tickets and links to ticketS?
+            - Use date from calendar to show this, (The hard part)
+
+            */}
             <label>Från</label>
             <input />
             <label>Till</label>
