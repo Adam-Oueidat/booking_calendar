388607c92fd601e027d349019b7a6bf4d5a4d55a
Adam Oueidat <ooueidat@gmail.com>
2024-06-18 23:41:20 +0200

    For now send them to SJ page, figure out if scraping is worth or not

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,11 +1,19 @@
 import Calendar from "@/src/components/calendar/Calendar";
+import Link from "next/link";
 
 export default function CalendarPage() {
   return (
     <>
       <div className="font-sans grid grid-rows-[20px_1fr_20px] justify-items-center w-full h-screen min-h-screen mr-24 p-8 pb-20 gap-16 sm:p-20">
         <div className="flex space-x-40">
-          <h1 className="text-4xl font-semibold text-center">Tåg biljetter</h1>
+          <div className="grid gap-10">
+            <h1 className="text-4xl font-semibold text-center">
+              Tåg biljetter
+            </h1>
+            <Link href="https://www.sj.se/sok-resa/sok/fran-station/till-station/datum-for-utresa">
+              Boka här
+            </Link>
+          </div>
           <Calendar />
         </div>
       </div>
