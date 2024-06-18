9c452374b35de7b4b63d851fccd66f2846e24bfd
Adam Oueidat <ooueidat@gmail.com>
2024-06-18 23:44:44 +0200

    I have an idea

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -6,10 +6,14 @@ export default function CalendarPage() {
     <>
       <div className="font-sans grid grid-rows-[20px_1fr_20px] justify-items-center w-full h-screen min-h-screen mr-24 p-8 pb-20 gap-16 sm:p-20">
         <div className="flex space-x-40">
-          <div className="grid gap-10">
+          <div className="grid gap-5  ">
             <h1 className="text-4xl font-semibold text-center">
               Tåg biljetter
             </h1>
+            <label>Från</label>
+            <input />
+            <label>Till</label>
+            <input />
             <Link href="https://www.sj.se/sok-resa/sok/fran-station/till-station/datum-for-utresa">
               Boka här
             </Link>
