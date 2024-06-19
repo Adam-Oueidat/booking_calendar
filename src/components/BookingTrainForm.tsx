c58d75c8b46367b55f94214dcc62447834e29a62
Adam Oueidat <ooueidat@gmail.com>
2024-06-19 22:05:00 +0200

    Add default value ?

--- src/components/BookingTrainForm.tsx
+++ src/components/BookingTrainForm.tsx
@@ -10,8 +10,10 @@ export default function BookingTrainForm() {
 
   return (
     <form action={formAction} className="grid gap-5">
-      <label>Från</label>
       <select name="city-from" id="city-from" className="text-black rounded">
+        <option disabled selected className="display:none">
+          Välj stad att åka ifrån
+        </option>
         <option value="gothenburg">Göteborg</option>
         <option value="stockholm">Stockholm</option>
         <option value="malmo">Malmö</option>
