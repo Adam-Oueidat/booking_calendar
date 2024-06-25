aad753a9d02f52603f6f86916f2c7767f006d6a8
Adam Oueidat <ooueidat@gmail.com>
2024-06-25 21:19:35 +0200

    Remove cache

--- config.ts
+++
@@ -1,9 +0,0 @@
-export const ALLOWED_DURATIONS = [15, 30, 60]
-
-export const DEFAULT_DURATION = 30
-
-export const CALENDARS_TO_CHECK = ["primary"]
-
-export const SLOT_PADDING = 0
-
-export const OWNER_TIMEZONE = "Europe/Stockholm"
