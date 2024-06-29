141ff34339330a9f3de056efafd70f0817834f16
Adam Oueidat <ooueidat@gmail.com>
2024-06-29 17:25:41 +0200

    Fix error with defaultValue for selected

--- src/components/BookingTrainForm.tsx
+++ src/components/BookingTrainForm.tsx
@@ -20,8 +20,9 @@ export default function BookingTrainForm() {
         dark:bg-gray-700 dark:border-gray-600 
         dark:placeholder-gray-400 dark:text-white
         dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        defaultValue="default"
       >
-        <option disabled selected className="display:none">
+        <option disabled value="default" className="display:none">
           Välj stad att åka ifrån
         </option>
         <option value="gothenburg">Göteborg</option>
