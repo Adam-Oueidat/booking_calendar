e89711b478490bdb1552ba7a0872e3e76476e5a6
Adam Oueidat <ooueidat@gmail.com>
2024-08-16 23:30:40 +0200

    Add block event for admin

--- src/components/modal/ModalForm.tsx
+++ src/components/modal/ModalForm.tsx
@@ -4,6 +4,8 @@ import { requestEvent } from "@/src/components/actions";
 import DateInput from "@/src/components/modal/DateInput";
 import TextInput from "@/src/components/modal/TextInput";
 import { EventContext } from "@/src/components/calendar/Month";
+import { useSession } from "next-auth/react";
+import { ADMIN_EMAIL } from "@/src/lib/util/utils";
 
 const initialState = false;
 type ModalFormProps = {
@@ -27,6 +29,8 @@ function SubmitButton() {
 export default function ModalForm({ date, closeModal }: ModalFormProps) {
   const [state, formAction] = useActionState(requestEvent, initialState);
   const { refreshing, setRefreshing } = useContext(EventContext);
+  const { data: session, status } = useSession();
+  const isAdmin = session?.user?.email === "ooueidat@gmail.com";
 
   useEffect(() => {
     if (state) {
@@ -75,6 +79,15 @@ export default function ModalForm({ date, closeModal }: ModalFormProps) {
       </div>
       <div className="relative bottom-0 right-0">
         <SubmitButton />
+        {status === "authenticated" && isAdmin ? (
+          <button
+            type="submit"
+            value="submit"
+            className="bg-red-600 float-end hover:bg-green-800 text-white font-bold py-2 px-4 mr-2 rounded"
+          >
+            Block event
+          </button>
+        ) : null}
       </div>
     </form>
   );
---
+++ src/lib/util/utils.ts
@@ -0,0 +1,3 @@
+"use server";
+
+export const ADMIN_EMAIL = process.env.NEXT_PUBLIC_AUTH_ADMIN_EMAIL;
