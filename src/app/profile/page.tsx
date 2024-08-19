7e50e377e22203d985996ac8352da90bc8efaa16
Adam Oueidat <ooueidat@gmail.com>
2024-08-19 21:33:57 +0200

    Remove image from profile, don't like it

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -6,39 +6,30 @@ export default async function Profile() {
   const user = session?.user;
 
   return (
-    <>
-      <section className="bg-ct-blue-600  min-h-screen pt-20">
-        <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-start items-start">
-          <div>
-            <p className="mb-3 text-5xl text-center font-semibold">
-              Profile Page
-            </p>
-            {!user ? (
-              <p>Loading...</p>
-            ) : (
-              <>
-                <div className="flex gap-24">
-                  <div className="">
-                    <Image
-                      src={user.image ? user.image : "/images/default.png"}
-                      className="max-h-36"
-                      width={144}
-                      height={144}
-                      alt={`profile photo of ${user.name}`}
-                    />
-                    <div className="mt-8">
-                      <p className="mb-3">Name: {user.name}</p>
-                      <p className="mb-3">Email: {user.email}</p>
-                    </div>
+    <section className="bg-ct-blue-600  min-h-screen pt-20">
+      <div className="max-w-4xl mx-auto bg-ct-dark-100 rounded-md h-[20rem] flex justify-start items-start">
+        <div className="ml-20">
+          <p className="mb-3 text-5xl text-center font-semibold">
+            Profile Page
+          </p>
+          {!user ? (
+            <p>Loading...</p>
+          ) : (
+            <>
+              <div className="flex">
+                <div className="">
+                  <div className="mt-8">
+                    <p className="mb-3">Name: {user.name}</p>
+                    <p className="mb-3">Email: {user.email}</p>
                   </div>
-
-                  <RequestedEventList userEmail={user.email ?? "Default"} />
                 </div>
-              </>
-            )}
-          </div>
+
+                <RequestedEventList userEmail={user.email ?? "Default"} />
+              </div>
+            </>
+          )}
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
 }
