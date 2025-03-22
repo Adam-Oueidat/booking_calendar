a1af76cae0bc77ad851177c258ecc42e085407c7
Adam Oueidat <ooueidat@gmail.com>
2025-03-22 23:21:30 +0100

    Loginpage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,9 +3,9 @@ import LoginForm from "@/src/components/authorization/Login";
 export default async function LoginPage() {
   return (
     <>
-      <section className="bg-ct-blue-600 min-h-screen pt-20">
+      <section className="bg-slate-900 min-h-screen pt-20">
         <div className="container mx-auto px-6 py-12 h-full flex justify-center items-center">
-          <div className="md:w-8/12 lg:w-5/12 rounded-lg bg-white px-8 py-10 ">
+          <div className="md:w-8/12 lg:w-5/12 rounded-lg bg-slate-800/50 backdrop-blur-lg px-8 py-10 border border-slate-700/50">
             <LoginForm />
           </div>
         </div>
--- src/components/authorization/Login.tsx
+++ src/components/authorization/Login.tsx
@@ -10,8 +10,7 @@ export default function LoginForm() {
     <>
       <Suspense fallback={<div>Loading...</div>}>
         <a
-          className="px-7 py-2 text-white font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3"
-          style={{ backgroundColor: "#ffffff", color: "gray" }}
+          className="px-7 py-2 text-slate-100 font-medium text-sm leading-snug uppercase rounded shadow-md hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full flex justify-center items-center mb-3 bg-slate-800/50 hover:bg-slate-700/50 border border-slate-700/50"
           onClick={() => signIn("google", { callbackUrl })}
           role="button"
         >
